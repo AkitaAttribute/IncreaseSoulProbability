0947fa4f63083c22470b5dca21e37a2612373e0e
AkitaAttribute <74178046+AkitaAttribute@users.noreply.github.com>
2024-04-14 05:55:04 -0400

    Changes to random.  Fixed some things to behave more so how I initially intended it to..
    Added a few functionality pieces for those who asked for it.

--- IncreaseSoulProbability.lua
+++ IncreaseSoulProbability.lua
@@ -6,39 +6,63 @@
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
--- 1 is 100% chance it will spawn Black Hole and Soul.  Increase the value to lower the chance. 100 would be a 1 in 100 chance.
--- 1 will be a 100% chance of spectral packs having both of these.  Planet packs will contain a black hole 100%, and Tarot packs will 100% contain a Soul.  
--- Oddly, it only marginally increases the chance of Black Hole appearing inside of Tarot packs.
--- I did very basic work here without complete understanding of the impacts.
+-- 1 is 100% chance it will spawn Soul.  Increase the value to lower the chance. 100 would be a 1 in 100 chance.
+-- 1 will be a 100% chance of spectral packs having Soul.
 -- Once all legendary cards are owned, they will start to spawn the default joker.  This is base game behavior.
-local chanceValue = 1 
+local chanceValue = 1
+-- Can be anything actually.  You can make vouchers show up in packs.
+local probUppedCard = "c_soul"
+-- local pack_types = {"Tarot", "Spectral", "Tarot_Planet"}
+local probUppedCardPacks = {"Spectral"} -- Made it this way to allow for more inclusions if desired.
 
 local originalFuncRef = create_card
 
 function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    local original_pseudorandom = pseudorandom
-    pseudorandom = function(key)
-        local original_result = original_pseudorandom(key)
+	-- Soul/BH logic
+    --local original_pseudorandom = pseudorandom
+    --pseudorandom = function(key)
+    --    local original_result = original_pseudorandom(key)
         
 		-- logToFile("Card key: " .. key)
-        if string.find(key, 'soul_') then
-			if math.random(chanceValue) == 1 then
-				return 1
-			end
-            return 0
-        end
+    --    if string.find(key, 'soul_') then
+	--		if math.random(chanceValue) == 1 then
+	--			return 1
+	--		end
+    --        return 0
+    --    end
         
-        return original_result
-    end
+    --    return original_result
+    --end
+	-- Soul/BH logic
+	
+	local newCardGen
+	if (type_condition(probUppedCardPacks, _type)) then
+		if math.random(chanceValue) == 1 then
+			newCardGen = originalFuncRef(_type, area, legendary, _rarity, skip_materialize, soulable, probUppedCard, key_append)
+		else
+			newCardGen = originalFuncRef(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+		end
+	else
+		newCardGen = originalFuncRef(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+	end
     
-    local newCardGen = originalFuncRef(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
-    
-    pseudorandom = original_pseudorandom
+	-- reset psuedorandom back to normal
+    --pseudorandom = original_pseudorandom
     
     return newCardGen
 end
 
 
+function type_condition(valid_types, input_string)
+	-- example valid_types input:
+	-- local pack_types = {"Tarot", "Spectral", "Tarot_Planet"}
+	for _, str in ipairs(valid_types) do
+        if str == input_string then
+            return true  -- The input string is in the list
+        end
+    end
+    return false  -- The input string is not in the list
+end
 
 
 local originalSplashScreenRef = G.splash_screen
@@ -47,8 +71,8 @@ function G.splash_screen(self)
 	
     local result = originalSplashScreenRef(self)
 	
-	-- forceTag("tag_ethereal")
-	-- forceTag("tag_charm")
+	forceTag("tag_ethereal")
+	--forceTag("tag_charm")
 	-- forceTag("tag_meteor")
 	
 	return result
